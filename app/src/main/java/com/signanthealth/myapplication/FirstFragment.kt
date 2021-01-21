806823a4576a52b62e8e02fbf53e47bb2882ac86
remmie.vail <remmie.vail@signanthealth.com>
2021-01-21 09:00:09 -0600

    Create calculator UI

--- app/src/main/java/com/signanthealth/myapplication/FirstFragment.kt
+++ app/src/main/java/com/signanthealth/myapplication/FirstFragment.kt
@@ -11,7 +11,7 @@ import androidx.navigation.fragment.findNavController
 /**
  * A simple [Fragment] subclass as the default destination in the navigation.
  */
-class FirstFragment : Fragment() {
+class FirstFragment : Fragment(), View.OnClickListener {
 
     override fun onCreateView(
             inflater: LayoutInflater, container: ViewGroup?,
@@ -24,8 +24,26 @@ class FirstFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
-        view.findViewById<Button>(R.id.button_first).setOnClickListener {
-            findNavController().navigate(R.id.action_FirstFragment_to_SecondFragment)
-        }
+        view.findViewById<Button>(R.id.one).setOnClickListener(this)
+        view.findViewById<Button>(R.id.two).setOnClickListener(this)
+        view.findViewById<Button>(R.id.three).setOnClickListener(this)
+        view.findViewById<Button>(R.id.four).setOnClickListener(this)
+        view.findViewById<Button>(R.id.five).setOnClickListener(this)
+        view.findViewById<Button>(R.id.six).setOnClickListener(this)
+        view.findViewById<Button>(R.id.seven).setOnClickListener(this)
+        view.findViewById<Button>(R.id.eight).setOnClickListener(this)
+        view.findViewById<Button>(R.id.nine).setOnClickListener(this)
+        view.findViewById<Button>(R.id.zero).setOnClickListener(this)
+        view.findViewById<Button>(R.id.decimal).setOnClickListener(this)
+        view.findViewById<Button>(R.id.add).setOnClickListener(this)
+        view.findViewById<Button>(R.id.minus).setOnClickListener(this)
+        view.findViewById<Button>(R.id.multiply).setOnClickListener(this)
+        view.findViewById<Button>(R.id.devide).setOnClickListener(this)
+        view.findViewById<Button>(R.id.mod).setOnClickListener(this)
+        view.findViewById<Button>(R.id.equals).setOnClickListener(this)
+    }
+
+    override fun onClick(v: View?) {
+        TODO("Not yet implemented")
     }
 }
